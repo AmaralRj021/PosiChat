fe5dfbe6404d807007a5abdeffb719a900a76998
Rodrigo Amaral <badmotherfckr021@gmail.com>
2025-07-28 18:40:23 +0100

    Corrige o caminho base para a publicação final

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chat-app/",
+  // A linha abaixo foi corrigida para garantir que o site funciona online
+  base: "/",
   build: {
     chunkSizeWarningLimit: 900,
   },
